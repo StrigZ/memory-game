047cc7a728cad14393122fb3bcf8f8c522be163b
StrigZ <mirgasan200@gmail.com>
2025-02-03 12:37:29 +0300

    Configure prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,15 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 80,
+  importOrder: ['^[^./~]', '^@?[^./~]', '^~', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
